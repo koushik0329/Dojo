d89d7f0b4062eeaa19c979f69516dedea5ba746a
koushik0329 <kkoushik_cse1905e2@mgit.ac.in>
2024-11-03 09:58:10 -0600

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,18 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:8000/blogs")
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       setBlogs(data); // Set fetched data to blogs
-  //     });
-  // }, []);
-
-  useEffect(() => {
-    fetch("http://localhost:800/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data); // Set fetched data to blogs
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  }, []);
+  const {
+    error,
+    isPending,
+    data: blogs,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-      {/* <BlogList
-        blogs={blogs.filter((blog) => blog.author == "mario")}
-        title="Mario's Blogs!"
-      /> */}
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
